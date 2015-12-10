91918ba8b2dbfe3b418fe7222024c87a534cca10
Tim Lukian <tim@hagbard.local>
2015-12-10 11:36:35 -0800

    roll back video change

--- web_root/wordpress/wp-content/themes/eclipseguy2015/single-videos.php
+++ web_root/wordpress/wp-content/themes/eclipseguy2015/single-videos.php
@@ -2,34 +2,34 @@
   <div class="single-video-player">
     <div class="embed-container">
       <?php
-        // the_field('eg_video_url');
+        the_field('eg_video_url');
 
-          $iframe = get_field('eg_video_url');
-          // use preg_match to find iframe src
-          preg_match('/src="(.+?)"/', $iframe, $matches);
-          $src = $matches[1];
-
-
-          // add extra params to iframe src
-          $params = array(
-              'controls'    => 0,
-              'hd'        => 1,
-              'autohide'    => 1
-          );
-
-          $new_src = add_query_arg($params, $src);
-
-          $iframe = str_replace($src, $new_src, $iframe);
+          // $iframe = get_field('eg_video_url');
+          // // use preg_match to find iframe src
+          // preg_match('/src="(.+?)"/', $iframe, $matches);
+          // $src = $matches[1];
 
 
-          // add extra attributes to iframe html
-          $attributes = 'frameborder="0"';
+          // // add extra params to iframe src
+          // $params = array(
+          //     'controls'    => 0,
+          //     'hd'        => 1,
+          //     'autohide'    => 1
+          // );
 
-          $iframe = str_replace('></iframe>', ' ' . $attributes . '></iframe>', $iframe);
+          // $new_src = add_query_arg($params, $src);
+
+          // $iframe = str_replace($src, $new_src, $iframe);
 
 
-          // echo $iframe
-          echo $iframe;
+          // // add extra attributes to iframe html
+          // $attributes = 'frameborder="0"';
+
+          // $iframe = str_replace('></iframe>', ' ' . $attributes . '></iframe>', $iframe);
+
+
+          // // echo $iframe
+          // echo $iframe;
 
       ?>
 
