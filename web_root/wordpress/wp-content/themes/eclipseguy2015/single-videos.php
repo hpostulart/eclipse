8709a947c2f91294bb53e56e975bcce245ed6eee
Tim Lukian <tim@hagbard.local>
2015-12-10 11:17:21 -0800

    update single video embedding for HD

--- web_root/wordpress/wp-content/themes/eclipseguy2015/single-videos.php
+++ web_root/wordpress/wp-content/themes/eclipseguy2015/single-videos.php
@@ -1,7 +1,39 @@
 <div class="row">
   <div class="single-video-player">
     <div class="embed-container">
-      <?php the_field('eg_video_url'); ?>
+      <?php
+        // the_field('eg_video_url');
+
+          $iframe = get_field('eg_video_url');
+          // use preg_match to find iframe src
+          preg_match('/src="(.+?)"/', $iframe, $matches);
+          $src = $matches[1];
+
+
+          // add extra params to iframe src
+          $params = array(
+              'controls'    => 0,
+              'hd'        => 1,
+              'autohide'    => 1
+          );
+
+          $new_src = add_query_arg($params, $src);
+
+          $iframe = str_replace($src, $new_src, $iframe);
+
+
+          // add extra attributes to iframe html
+          $attributes = 'frameborder="0"';
+
+          $iframe = str_replace('></iframe>', ' ' . $attributes . '></iframe>', $iframe);
+
+
+          // echo $iframe
+          echo $iframe;
+
+      ?>
+
+
     </div>
   </div>
 
