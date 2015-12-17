240f1352e354701a2fad173adb854ffc3b1b2914
Tim Lukian <tim@hagbard.local>
2015-12-16 16:09:50 -0800

    pull and regen images, fix bad blurbs on post or report related content items

--- web_root/wordpress/wp-content/themes/eclipseguy2015/lib/custom.php
+++ web_root/wordpress/wp-content/themes/eclipseguy2015/lib/custom.php
@@ -48,3 +48,45 @@ function remove_post_formats() {
    remove_theme_support( 'post-formats' );
     // add_theme_support( 'post-formats', array( 'link', 'gallery' ) );
 }
+
+
+
+
+
+
+
+//custom excerpt by id function
+/**
+ * Get the excerpt of a specific post ID or object
+ *
+ * @author Pippin Williamson
+ * @link http://goo.gl/lhtZD
+ * @param object/int $post The ID or object of the post to get the excerpt of
+ * @param int $length The length of the excerpt in words
+ * @param string $tags The allowed HTML tags. These will not be stripped out
+ * @param string $extra Text to append to the end of the excerpt
+ */
+function eg_get_excerpt_by_id( $post, $length = 10, $tags = '<a><em><strong>', $extra = '&hellip;' ) {
+
+  if ( is_numeric( $post ) ) {
+    $post = get_post( $post );
+  } elseif( ! is_object( $post ) ) {
+    return false;
+  }
+
+  if ( has_excerpt( $post->ID ) ) {
+    $the_excerpt = $post->post_excerpt;
+    return apply_filters( 'the_content', $the_excerpt );
+  } else {
+    $the_excerpt = $post->post_content;
+  }
+
+  $the_excerpt = strip_shortcodes( strip_tags( $the_excerpt, $tags ) );
+  $the_excerpt = preg_split( '/\b/', $the_excerpt, $length * 2 + 1 );
+  $excerpt_waste = array_pop( $the_excerpt );
+  $the_excerpt = implode( $the_excerpt );
+  $the_excerpt .= $extra;
+
+  // return apply_filters( 'the_content', $the_excerpt );
+  return $the_excerpt;
+}
--- web_root/wordpress/wp-content/themes/eclipseguy2015/templates/sidebar-videos-single.php
+++ web_root/wordpress/wp-content/themes/eclipseguy2015/templates/sidebar-videos-single.php
@@ -30,13 +30,15 @@
           <?php
             switch ($posttype) {
                 case 'reports':
-                    the_excerpt();
+                    // the_excerpt();
+                    echo eg_get_excerpt_by_id( $feat_item->ID , 99 );
                     break;
                 case 'videos':
-                    echo get_field('eg_video_description_short',$post->ID);
+                    echo get_field('eg_video_description_short',$feat_item->ID);
                     break;
                 case 'post':
-                    the_excerpt();
+                    // the_excerpt();
+                    echo eg_get_excerpt_by_id( $feat_item->ID , 99 );
                     break;
             }
           ?>
