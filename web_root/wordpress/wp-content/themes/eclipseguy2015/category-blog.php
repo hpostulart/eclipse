4b913bae9aec4e0840e79a09da201bfb3994013c
Tim Lukian <tim@hagbard.local>
2015-12-21 16:00:54 -0800

    fix blog category archive view

---
+++ web_root/wordpress/wp-content/themes/eclipseguy2015/category-blog.php
@@ -0,0 +1,33 @@
+<?php while (have_posts()) : the_post(); ?>
+
+  <?php
+    // get_template_part('templates/content', 'page');
+  ?>
+
+  <div class="page-header">
+    <div class="row">
+      <div class="col-sm-12">
+        <h2 class=""><?= __('Eclipseguy Blog','eg'); ?></h2>
+      </div>
+    </div>
+  </div>
+
+  <!-- <div class="row"> -->
+    <div class="linearwrapper" >
+      <?php
+        $args = array(
+          // "post_type"        => 'post',
+          // "posts_per_page"   => 12,
+          // "order"            => 'desc'
+        );
+
+        // get_template_part("templates/content","linear-listing");
+      ?>
+
+      <?php
+        $loadmore_shortcode = '[ajax_load_more repeater="template_2" post_type="post" category="blog" posts_per_page="5" scroll_distance="200" max_pages="99" button_label="Load More Posts"]';
+        echo do_shortcode( $loadmore_shortcode );
+      ?>
+    </div>
+  <!-- </div> -->
+<?php endwhile; ?>
